b22f5472068876bea75f315583e4803ed10ca1a4
Lala Sabathil <aiko@aitsys.dev>
2025-09-07 16:18:58 +0200

    Restructure project into src and linux directories
    Moved all source files into a new src directory and deployment scripts/services into a linux directory. Updated solution and publish script paths to reflect the new structure. Added LICENSE and README files to the solution.

---
+++ linux/publish.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet publish -c Release -r linux-x64 --self-contained=true -p:PublishSingleFile=false -p:GenerateRuntimeConfigurationFiles=true -o /opt/bots/discord-lib-dev-tracking ../src/AITSYS.Discord.LibraryDevelopmentTracking.csproj
--- publish_linux.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dotnet publish -c Release -r linux-x64 --self-contained=true -p:PublishSingleFile=false -p:GenerateRuntimeConfigurationFiles=true -o /opt/bots/discord-lib-dev-tracking AITSYS.Discord.LibraryDevelopmentTracking.csproj
